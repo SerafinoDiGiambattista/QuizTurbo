ff839b7be7d9ee77a5150bb2461d3390f71992df
SerafinoDiGiambattista <s.digiambattista1@studenti.unisannio.it>
2023-01-20 17:48:50 +0100

    Add trigger for question

--- Assets/Prototipo/Script/GeneratorLevel/ManagerSpawner.cs
+++ Assets/Prototipo/Script/GeneratorLevel/ManagerSpawner.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Runtime.CompilerServices;
-
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -11,6 +12,8 @@ public class ManagerSpawner : MonoBehaviour
     [SerializeField] string fileInput;
 
     [SerializeField] GameObject road;
+    [SerializeField] GameObject canvasQuestion;
+    private TextMeshProUGUI textmeshPro;
     Vector3 nextSpawnPoint = new Vector3 (0,0,0);
     public float zpos = 30f;
     public int index = 5;
@@ -23,16 +26,17 @@ public class ManagerSpawner : MonoBehaviour
       
         GameObject temp = Instantiate(road, nextSpawnPoint, Quaternion.identity);
         if (count == 3)
-        {   
-           
-            temp.GetComponent<SlideRoad>().SpawnObstacle(false);
+        {
             
+            temp.GetComponent<SlideRoad>().SpawnObstacle(false);
+         
            
 
         }
         else if (count == 4)
         {
             //temp.GetComponent<SlideRoad>().SetActivateCanvas(false);
+
             count = 0;
         }
         else
@@ -40,6 +44,19 @@ public class ManagerSpawner : MonoBehaviour
         //temp.GetComponent<SlideRoad>().SpawnPanel();
     }
 
+    public void SpwanQuestion(Boolean pass)
+    {
+        if (pass) {
+            ReadCSV();
+            canvasQuestion.SetActive(pass);
+        }
+        else
+        {
+            canvasQuestion.SetActive(pass);
+        }
+    }
+     
+    
 
     public float StartSpwan(float position, Boolean spawnItems)
     {
@@ -54,11 +71,65 @@ public class ManagerSpawner : MonoBehaviour
         return position;
     }
 
-    private void Start()
+    private void ReadCSV()
     {
+        StreamReader strReader = new StreamReader(fileInput);
+
+        Dictionary<int, string> tabellaDomande = new Dictionary<int, string>();
+        Dictionary<int, int> tabellaRisposte = new Dictionary<int, int>();
+
+        bool endOdFile = false;
+        int i = 0;
+
+        while (!endOdFile)
+        {
+            string data_String = strReader.ReadLine();
+            if (data_String == null)
+            {
+                endOdFile = true;
+                break;
+            }
+            string[] row = data_String.Split(';');
+            tabellaDomande.Add(i, row[0]);
+            tabellaRisposte.Add(i, int.Parse(row[1]));
+            // Debug.Log(tabellaDomande[i]+ " risposta: "+ tabellaRisposte[i]);  
+            i++;
+
+        }
+
+        int sizeTabella = i;
+        //Debug.Log("i: " + i);
+
+        int rand = UnityEngine.Random.Range(0, sizeTabella); // 0 è compreso, sizeTabella non è compreso
+        //Debug.Log("random number: " + rand);
+        while (!tabellaDomande.ContainsKey(rand))
+            rand = UnityEngine.Random.Range(0, sizeTabella);
+
+        string domanda = tabellaDomande[rand];
+        tabellaDomande.Remove(rand);
+
+        textmeshPro.SetText(domanda, true);
+        //Debug.Log("Valore di text : "+textmeshPro);
+
+        //int risposta = tabellaRisposte[rand];
+        //tabellaRisposte.Remove(rand);    
+
+        // yield return new WaitForSeconds(0.5f);
+
+
+
+    }
+
+    private void Start()
+    {    
+        canvasQuestion = Instantiate(canvasQuestion);
+         textmeshPro = (TextMeshProUGUI)canvasQuestion.GetComponentInChildren(typeof(TextMeshProUGUI));
         float position = 0;
+        //canvasQuestion.SetActive(true);
         //disattivate il primo trigger
         Instantiate(road, nextSpawnPoint, Quaternion.identity);
+        
+        
         for (int i=0; i<index; i++)
         {
             if (i < index/2)
--- Assets/Prototipo/Script/GeneratorLevel/SlideRoad.cs
+++ Assets/Prototipo/Script/GeneratorLevel/SlideRoad.cs
@@ -1,12 +1,6 @@
 
 using System;
 using UnityEngine;
-using TMPro;
-using System.IO;
-using System.Collections.Generic;
-using System.Collections;
-using static UnityEngine.UI.Image;
-using Unity.MLAgents;
 
 public class SlideRoad : MonoBehaviour
 {
@@ -16,36 +10,34 @@ public class SlideRoad : MonoBehaviour
     [SerializeField] GameObject obstaclePrefab;
     [SerializeField] GameObject panelTrue;
     [SerializeField] GameObject panelFalse;
-    [SerializeField] GameObject canvasDomanda;
-    public TextMeshProUGUI textmeshPro;
     public string fileInput;
-    private int count = 0;
+    private Collider checkpoint;
+
 
     private void Start()
     {
         roadSpawner = GameObject.FindObjectOfType<ManagerSpawner>();
+       
+
     }
 
     private void OnTriggerExit(Collider other)
     {
         //chiedere a caturano perchè succede ??
-        SetActivateCanvas(true);
-                
+
             if (other.gameObject.CompareTag("Ciccio"))
             {   
                 roadSpawner.SpawnSegment();
+                roadSpawner.SpwanQuestion(false);
                 Destroy(gameObject, 1);
                 
-            
-                
                    // ReadCSV();
-                    
-             
                 
             }
         
        
     }
+    
 
 
     public void SpawnObstacle(Boolean spawOb)
@@ -83,66 +75,30 @@ public class SlideRoad : MonoBehaviour
 
         Transform spawnFalse = transform.Find("False").transform;
         Instantiate(panelFalse, spawnFalse.position, panelFalse.transform.rotation, transform);
-        
+
+        checkpoint =  transform.Find("checkpoint").GetComponent<Collider>();
+        checkpoint.enabled=true;
+
     }
 
-    public void SetActivateCanvas(Boolean activate)
+    private void OnTriggerEnter(Collider other)
     {
-
-       
-        canvasDomanda.SetActive(activate);
-        Debug.Log("Canvas : "+canvasDomanda);
-    }
-
-
-    public void ReadCSV()
-    {
-        StreamReader strReader = new StreamReader(fileInput);
-
-        Dictionary<int, string> tabellaDomande = new Dictionary<int, string>();
-        Dictionary<int, int> tabellaRisposte = new Dictionary<int, int>();
-
-        bool endOdFile = false;
-        int i = 0;
-
-        while (!endOdFile)
+        if (transform.Find("checkpoint").GetComponent<Collider>().enabled == true)
         {
-            string data_String = strReader.ReadLine();
-            if (data_String == null)
+            if (other.gameObject.CompareTag("Ciccio"))
             {
-                endOdFile = true;
-                break;
+              
+                Debug.Log("Checkpoint ");
+                roadSpawner.SpwanQuestion(true);
+                transform.Find("checkpoint").GetComponent<Collider>().enabled = false;
+
             }
-            string[] row = data_String.Split(';');
-            tabellaDomande.Add(i, row[0]);
-            tabellaRisposte.Add(i, int.Parse(row[1]));
-           // Debug.Log(tabellaDomande[i]+ " risposta: "+ tabellaRisposte[i]);  
-            i++;
-
+            
         }
-
-        int sizeTabella = i;
-        //Debug.Log("i: " + i);
-
-        int rand = UnityEngine.Random.Range(0, sizeTabella); // 0 è compreso, sizeTabella non è compreso
-        //Debug.Log("random number: " + rand);
-        while (!tabellaDomande.ContainsKey(rand))
-            rand = UnityEngine.Random.Range(0, sizeTabella);
-
-        string domanda = tabellaDomande[rand];
-        tabellaDomande.Remove(rand);
-
-        textmeshPro.SetText(domanda,true);
-        Debug.Log("Valore di text : "+textmeshPro);
-
-        //int risposta = tabellaRisposte[rand];
-        //tabellaRisposte.Remove(rand);    
-
-       // yield return new WaitForSeconds(0.5f);
-        
-
-
     }
+
+
+
 
     private void FixedUpdate()
     {
