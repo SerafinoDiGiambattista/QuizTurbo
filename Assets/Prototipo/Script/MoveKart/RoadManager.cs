f7215c98262b539c8eacd540790009de81715bf8
SerafinoDiGiambattista <s.digiambattista1@studenti.unisannio.it>
2023-01-28 12:09:07 +0100

    Track Road Bounding box

--- Assets/Prototipo/Script/MoveKart/RoadManager.cs
+++ Assets/Prototipo/Script/MoveKart/RoadManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class RoadManager : MonoBehaviour
 {
     /// <summary>
@@ -9,10 +10,40 @@ public class RoadManager : MonoBehaviour
     /// Il compito princiaple è decidere cosa fa spwanare sulla strada: strada vuota, ostacoli e power up oppure domande 
     /// </summary>
     // Start is called before the first frame update
+    protected FeatureManager featureManager;
+    protected ComponentManager componentManager;
+    [SerializeField] protected List<string> collidersTags;
+    protected ObstaclesPowerUp obstaclesPowerUp;
+    [SerializeField] GameObject track_road;
 
+
+    //protected QuestionManager questionManager;
+
+    private void Awake()
+    {
+        featureManager = GetComponent<FeatureManager>();
+        componentManager = GetComponent<ComponentManager>();
+        obstaclesPowerUp = GetComponent<ObstaclesPowerUp>();
+    }
     void Start()
     {
-        
+        IstatiateRoad();
+    }
+
+    private  void IstatiateRoad()
+    {
+        Renderer renderer= track_road.GetComponent<Renderer>();
+        if (renderer != null)
+        {   //lunghezza z dell'oggetto
+            float lenghtz = renderer.bounds.size.z;
+           
+            float count = 0;
+            for (int i =0; i<10; i++)
+            {
+                Instantiate(track_road, new Vector3(0,0,count), Quaternion.identity) ;
+                count += lenghtz;
+            }
+        }
     }
 
     // Update is called once per frame
